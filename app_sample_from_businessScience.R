af0c898755841b03836e34fd5a649e83a2333dd7
S Lee <sanlee8495@gmail.com>
2022-07-10 09:50:46 -0700

    7/10/22

--- app_sample_from_businessScience.R
+++ app_sample_from_businessScience.R
@@ -1,30 +1,11 @@
-# R TIPS ----
-# TIP 027 | Gentle Introduction to Shiny ---
-#
-# 👉 For Weekly R-Tips, Sign Up Here: https://mailchi.mp/business-science/r-tips-newsletter
-
-# Why Shiny?
-# - Example of Nostradamus (My Forecasting App built with modeltime)
-#   https://business-science.shinyapps.io/nostradamus/
-
-# LIBRARIES ----
-
-# Shiny
 library(shiny)
 library(bslib)
-
-# Modeling
 library(modeldata)
 library(DataExplorer)
-
-# Widgets
 library(plotly)
-
-# Core
 library(tidyverse)
 
-
-# LOAD DATASETS ----
+# dataset loading
 utils::data("stackoverflow", "car_prices", "Sacramento", package = "modeldata")
 
 data_list = list(
@@ -33,32 +14,23 @@ data_list = list(
   "Sacramento Housing" = Sacramento
 )
 
-# 1.0 USER INTERFACE ----
+# user interface
 ui <- navbarPage(
-  
   title = "Data Explorer",
-  
   theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
-  
   tabPanel(
     title = "Explore",
-    
     sidebarLayout(
-      
       sidebarPanel(
         width = 3,
         h1("Explore a Dataset"),
         
-        # Requires Reactive Programming Knowledge
-        # - Taught in Shiny Dashboards (DS4B 102-R)
         shiny::selectInput(
           inputId = "dataset_choice",
           label   = "Data Connection",
           choices = c("StackOverflow", "Car Prices", "Sacramento Housing")
         ),
         
-        # Requires Boostrap Knowledge
-        # - Taught in Shiny Developer with AWS (DS4B 202A-R)
         hr(),
         h3("Apps by Business Science"),
         p("Go from beginner to building full-stack shiny apps."),
@@ -69,8 +41,6 @@ ui <- navbarPage(
             class = "btn btn-lg btn-primary"
           ) %>%
           div(class = "text-center")
-        
-        
       ),
       
       mainPanel(
@@ -84,28 +54,17 @@ ui <- navbarPage(
   
 )
 
-# 2.0 SERVER ----
+# server ----
 server <- function(input, output) {
-  
-  # REACTIVE PROGRAMMING  ----
-  # - Taught in Shiny Dashboards & Shiny Developer Courses
-  
   rv <- reactiveValues()
-  
   observe({
-    
     rv$data_set <- data_list %>% pluck(input$dataset_choice)
-    
   })
-  
   output$corrplot <- renderPlotly({
-    
     g <- DataExplorer::plot_correlation(rv$data_set)
-    
     plotly::ggplotly(g)
   })
-  
 }
 
-# Run the application
+# run the app
 shinyApp(ui = ui, server = server)
